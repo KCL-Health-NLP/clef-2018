976e265b7969270a3d01c64f3110208d84434469
Natalia <36882562+nataliaresearch@users.noreply.github.com>
2018-05-02 10:06:32 +0100

    Add files via upload

---
+++ order_codes_integration.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import ast
+
+###create dictionary with frequencies###
+def create_dic():
+    folder = "C:/Users/Natalia/Documents/KingsWork/CLEF-2018/training_CLEFeHealth2018_FR/FR/"
+    textf = folder+'training/aligned/corpus/AlignedCauses_2006-2012full.csv'
+    df_training = pd.read_csv(textf, sep=';', error_bad_lines=False, warn_bad_lines=True) 
+    df_training = df_training.fillna("NULL")
+    df_training_icds = df_training.groupby("ICD10").size().reset_index(name="counts")
+    df_training_icds = df_training_icds.sort_values(by=["counts"], ascending = False)    
+    #dictionary with codes from training (ordered by counts)
+    dic={}
+    pos=1
+    for index, row in df_training_icds.iterrows():
+        code = row["ICD10"]
+        dic[code] = pos
+        pos = pos+1
+    return dic
+
+
+#### for each line in output ####
+def order_line(dic, icd_codes):
+    #create small dic
+    d2 = {}
+    unseen = []
+    for cod in icd_codes:
+    		if cod in dic:
+    			d2[dic[cod]]=cod
+    		else:
+    			unseen.append(cod)
+    #add unseen codes
+    last_pos =  len(dic)
+    for cod in unseen:   
+        d2[last_pos]=cod
+        last_pos = last_pos + 1
+    #create ordered list
+    d2_ordered = sorted(d2)
+    sorted_list = []
+    for position in d2_ordered:
+        sorted_list.append(d2[position])        
+    return sorted_list
+    
+dic = create_dic()
+icd_codes = "['ABC','I10', 'R092', 'X09', 'CDE']"
+icd_codes = icd_codes.replace("nan", "''")
+icd_codes = ast.literal_eval(icd_codes)
+icd_codes = list(filter(None, icd_codes))
+sorted_list = order_line(dic, icd_codes)
